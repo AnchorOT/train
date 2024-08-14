562ce3ee63f0edaf61b1adcd6a2194ebbea538e1
Anchor <fkw799@gmail.com>
2024-08-14 21:57:51 +0800

    初始化

---
+++ src/test/java/com/anchor/train/TrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anchor.train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
