9b08f0df39819eed5d123b8d5ae25bb9df8cd303
Anchor <fkw799@gmail.com>
2024-08-19 22:35:50 +0800

    5.6 将后端接口改为body json方式传递参数，前后端联调成功

--- member/src/main/java/com/anchor/train/member/controller/MemberController.java
+++ member/src/main/java/com/anchor/train/member/controller/MemberController.java
@@ -1,13 +1,13 @@
 package com.anchor.train.member.controller;
 
+import com.anchor.train.common.resp.CommonResp;
 import com.anchor.train.member.req.MemberLoginReq;
 import com.anchor.train.member.req.MemberRegisterReq;
 import com.anchor.train.member.req.MemberSendCodeReq;
 import com.anchor.train.member.resp.MemberLoginResp;
 import com.anchor.train.member.service.MemberService;
-import com.anchor.train.common.resp.CommonResp;
 import jakarta.annotation.Resource;
-import org.springframework.validation.annotation.Validated;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -26,7 +26,7 @@ public class MemberController {
     }
 
     @PostMapping("/register")
-    public CommonResp<Long> register(@Validated MemberRegisterReq req) {
+    public CommonResp<Long> register(@Valid MemberRegisterReq req) {
         long register = memberService.register(req);
 //        CommonResp<Long> commonResp = new CommonResp<Long>();
 //        commonResp.setContent(register);
@@ -34,12 +34,12 @@ public class MemberController {
         return new CommonResp<>(register);
     }
     @PostMapping("/send-code")
-    public CommonResp<Long> sendCode(@Validated MemberSendCodeReq req) {
+    public CommonResp<Long> sendCode(@Valid @RequestBody MemberSendCodeReq req) {
         memberService.SendCode(req);
         return new CommonResp<>();
     }
     @PostMapping("/login")
-    public CommonResp<MemberLoginResp> login(@Validated MemberLoginReq req) {
+    public CommonResp<MemberLoginResp> login(@Valid @RequestBody MemberLoginReq req) {
         MemberLoginResp resp = memberService.login(req);
         return new CommonResp<>(resp);
     }
