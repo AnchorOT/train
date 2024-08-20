5868ad27aacc8d606d5b8a41eb841a30f8e0cd4f
Anchor <fkw799@gmail.com>
2024-08-20 21:48:30 +0800

    5.8 使用hutool生成JWT token

--- member/src/main/java/com/anchor/train/member/service/MemberService.java
+++ member/src/main/java/com/anchor/train/member/service/MemberService.java
@@ -4,6 +4,7 @@ import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.RandomUtil;
+import cn.hutool.jwt.JWTUtil;
 import com.anchor.train.common.exception.BusinessException;
 import com.anchor.train.common.exception.BusinessExceptionEnum;
 import com.anchor.train.common.util.SnowUtil;
@@ -20,6 +21,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class MemberService {
@@ -78,7 +80,12 @@ public class MemberService {
         }
         //校验短信验证码
         if("8888".equals(code)){
-            return BeanUtil.copyProperties(memberDB, MemberLoginResp.class);
+            MemberLoginResp resp = BeanUtil.copyProperties(memberDB, MemberLoginResp.class);
+            Map<String, Object> map  = BeanUtil.beanToMap(resp);
+            String key = "Anchor12306";
+            String token = JWTUtil.createToken(map, key.getBytes());
+            resp.setToken(token);
+            return resp;
         }else {
             throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_CODE_ERROR);
         }
