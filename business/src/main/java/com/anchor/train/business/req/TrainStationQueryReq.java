7e175308723d6d4532121124dbeaed11125e00af
Anchor <fkw799@gmail.com>
2024-08-23 22:43:22 +0800

    7.5 增加火车车站表 生成持久层代码及前后端代码

---
+++ business/src/main/java/com/anchor/train/business/req/TrainStationQueryReq.java
@@ -0,0 +1,12 @@
+package com.anchor.train.business.req;
+
+import com.anchor.train.common.req.PageReq;
+
+public class TrainStationQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "TrainStationQueryReq{" +
+                "} " + super.toString();
+    }
+}
