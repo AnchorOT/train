9b130420880fcf0e28053cacb66d799bc723b4d8
Anchor <fkw799@gmail.com>
2024-08-23 22:53:14 +0800

    7.6 增加火车车厢表 生成持久层代码及前后端代码

---
+++ business/src/main/java/com/anchor/train/business/req/TrainCarriageQueryReq.java
@@ -0,0 +1,12 @@
+package com.anchor.train.business.req;
+
+import com.anchor.train.common.req.PageReq;
+
+public class TrainCarriageQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "TrainCarriageQueryReq{" +
+                "} " + super.toString();
+    }
+}
