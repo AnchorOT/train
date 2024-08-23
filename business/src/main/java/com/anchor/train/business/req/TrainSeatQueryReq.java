0836e2dcfc7bc2f70d654499579cb5f0842145af
Anchor <fkw799@gmail.com>
2024-08-23 23:02:58 +0800

    7.7 增加火车座位表 生成持久层代码及前后端代码

---
+++ business/src/main/java/com/anchor/train/business/req/TrainSeatQueryReq.java
@@ -0,0 +1,12 @@
+package com.anchor.train.business.req;
+
+import com.anchor.train.common.req.PageReq;
+
+public class TrainSeatQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "TrainSeatQueryReq{" +
+                "} " + super.toString();
+    }
+}
