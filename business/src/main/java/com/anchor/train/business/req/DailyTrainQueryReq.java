cb1fe6e6bd48cdee5a5688fa30c60d0e5e411e27
Anchor <fkw799@gmail.com>
2024-08-25 23:12:56 +0800

    9.1 生成每日车次数据管理功能

---
+++ business/src/main/java/com/anchor/train/business/req/DailyTrainQueryReq.java
@@ -0,0 +1,12 @@
+package com.anchor.train.business.req;
+
+import com.anchor.train.common.req.PageReq;
+
+public class DailyTrainQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "DailyTrainQueryReq{" +
+                "} " + super.toString();
+    }
+}
