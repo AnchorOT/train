11ac80adf73eec0a2417a2194a05ada701482d36
Anchor <fkw799@gmail.com>
2024-08-25 21:57:54 +0800

    8.5 z增加quartz相关的十二张表，增加接口操作quartz

--- sql/batch.sql
+++ sql/batch.sql
@@ -6,15 +6,6 @@
 # In your Quartz properties file, you'll need to set
 # org.quartz.jobStore.driverDelegateClass = org.quartz.impl.jdbcjobstore.StdJDBCDelegate
 #
-drop table if exists `member`;
-create table `member` (
-                          `id` bigint not null comment 'id',
-                          `mobile` varchar(11) comment '手机号',
-                          primary key (`id`),
-                          unique key `mobile_unique` (`mobile`)
-) engine=innodb default charset=utf8mb4 comment='会员';
-
-
 DROP TABLE IF EXISTS QRTZ_FIRED_TRIGGERS;
 DROP TABLE IF EXISTS QRTZ_PAUSED_TRIGGER_GRPS;
 DROP TABLE IF EXISTS QRTZ_SCHEDULER_STATE;
