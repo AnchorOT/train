94cb70ed8abef6435899c4c425a382e48d2afc9b
Anchor <fkw799@gmail.com>
2024-08-26 23:30:25 +0800

    9.10 增加生成每日车次功能

--- batch/src/main/java/com/anchor/train/batch/job/DailyTrainJob.java
+++ batch/src/main/java/com/anchor/train/batch/job/DailyTrainJob.java
@@ -1,6 +1,11 @@
 package com.anchor.train.batch.job;
 
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.RandomUtil;
+import com.anchor.train.batch.feign.BusinessFeign;
+import com.anchor.train.common.resp.CommonResp;
+import jakarta.annotation.Resource;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -8,12 +13,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
+import java.util.Date;
+
 public class DailyTrainJob implements Job {
+    @Resource
+    BusinessFeign businessFeign;
     private static final Logger LOG = LoggerFactory.getLogger(DailyTrainJob.class);
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         MDC.put("LOG_ID", System.currentTimeMillis() + RandomUtil.randomString(3));
         LOG.info("开始生成每日车次数据");
-        LOG.info("生成每日车次数据结束");
+        Date date = new Date();
+        DateTime dateTime = DateUtil.offsetDay(date, 15);
+        Date jdkDate = dateTime.toJdkDate();
+        CommonResp<Object> commonResp = businessFeign.genDaily(jdkDate);
+        LOG.info("生成每日车次数据结束  15天后 {}", commonResp);
     }
 }
