d9b262d46a4fa5755c736e975ba44f2dae275ade
Anchor <fkw799@gmail.com>
2024-08-26 20:10:56 +0800

    9.3 生成每日车站功能 并增加日期和车次查询条件

---
+++ business/src/main/java/com/anchor/train/business/req/DailyTrainStationQueryReq.java
@@ -0,0 +1,41 @@
+package com.anchor.train.business.req;
+
+import com.anchor.train.common.req.PageReq;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+public class DailyTrainStationQueryReq extends PageReq {
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+
+    /**
+     * 车次编号
+     */
+    private String trainCode;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public void setTrainCode(String trainCode) {
+        this.trainCode = trainCode;
+    }
+
+    @Override
+    public String toString() {
+        return "DailyTrainStationQueryReq{" +
+                "date=" + date +
+                ", trainCode='" + trainCode + '\'' +
+                "} " + super.toString();
+    }
+}
